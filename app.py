c006278e9cab2ac5f4eeb91618300d26f292ba23
surichrist <141798823+surichrist@users.noreply.github.com>
2024-09-13 09:48:02 +0530

    Update app.py

--- app.py
+++ app.py
@@ -121,5 +121,5 @@ if st.button("Submit Measurements"):
 
 
 
-st.markdown(''':rainbow[Developed eating]''' ":hamburger:" '''by Raghav''')
+st.markdown(''':rainbow[Developed eating]''' ":hamburger:")
 
