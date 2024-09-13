67fd33ac8c15d38d11ab61667c8054b440c9041a
surichrist <141798823+surichrist@users.noreply.github.com>
2024-09-13 09:11:23 +0530

    Update app.py

--- app.py
+++ app.py
@@ -119,4 +119,27 @@ if st.button("Submit Measurements"):
     st.write(f"Counts for |10⟩: {counts_10}")
     st.write(f"Counts for |11⟩: {counts_11}")
 
-    
+    > footer="""<style>
+a:link , a:visited{
+color: blue;
+background-color: transparent;
+text-decoration: underline;
+}
+
+a:hover,  a:active {
+color: red;
+background-color: transparent;
+text-decoration: underline;
+}
+
+.footer {
+position: fixed;
+left: 0;
+bottom: 0;
+width: 100%;
+background-color: white;
+color: black;
+text-align: center;
+}
+</style>
+<div class="footer">
